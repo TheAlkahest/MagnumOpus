c10d03e42f7b39587e186893a4597766ca363fcc
‘Zach <zstone@redkiteproject.com>
2019-12-10 15:40:08 -0500

    Scraper with BeautifulSoup to parse and fetch

--- scraper.py
+++ scraper.py
@@ -1,6 +1,20 @@
 import requests
+# add import of BeautifulSoup
+from bs4 import BeautifulSoup
+import time
 
 if __name__ == '__main__':
     response = requests.get("http://andythemoron.com")
-    # Let's print out the response for now to make sure we got some valid data back
-    print(response.text)
+    # Add use of BeautifulSoup to parse response
+    soup = BeautifulSoup(response.content, "lxml")
+    # Now we print the BeautifulSoup Object
+    post_titles = soup.find_all("a", class_="post-title")
+    extracted = []
+    for post_title in post_titles:
+        extracted.append({
+            'title' : post_title['title'],
+            'link' : "andythemoron.com" + post_title['href'] 
+        })
+        #Verify that data
+        for data in extracted:
+            print(data)
